814f0ac978b04c0b19b744104fbd183502ada15c
young-sang <68852430+young-sang@users.noreply.github.com>
2025-02-18 22:16:10 +0900

    DB 도안 추가

--- StoryAppDB.sql
+++ StoryAppDB.sql
@@ -33,3 +33,22 @@ create table novelitems (
     characters int,
     comment varchar(100)
 )
+
+-- 테이블 컬럼 추가
+
+-- ALTER TABLE 테이블명 ADD COLUMN 컬럼명 데이터타입;
+
+ALTER TABLE users ADD COLUMN age INT;
+
+-- 이미지 파일 저장 방법 / 파일을 서버에 저장하고 경로를 저장하는 방법 
+CREATE TABLE images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    file_path VARCHAR(500) NOT NULL
+);
+
+
+INSERT INTO images (name, file_path) 
+VALUES ('example.png', '/var/www/images/example.png');
+
+SELECT file_path FROM images WHERE id = 1;
