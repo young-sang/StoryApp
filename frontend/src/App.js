2326451d210817aafd344716e0ad99de17f0c246
young-sang <68852430+young-sang@users.noreply.github.com>
2025-02-19 22:05:27 +0900

    편집 route 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ function App() {
           <li><Link to="/ani">애니</Link></li>
           <li><Link to="/manga">만화</Link></li>
           <li><Link to="/novel">라이트노벨</Link></li>
+          <li><Link to="/addItem">추가</Link></li>
         </ul>
       </nav>
       <Routes>
@@ -23,6 +24,8 @@ function App() {
         <Route path="/ani" element={<ItemList title={"ani"} db={"aniitems"} />} />
         <Route path="/manga" element={<ItemList title={"manga"} db={"mangaitems"} />} />
         <Route path="/novel" element={<ItemList title={"Novel"} db={"novelitems"} />} />
+        <Route path="/addItem" element={<ItemControl mode={"add"} />} />
+        <Route path="/updateItem" element={<ItemControl mode={"update"} />} />
       </Routes>
     </Router>
   );
