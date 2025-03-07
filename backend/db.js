9228cc2714cf9a93dd004d3570ac6e2020cc8023
young-sang <68852430+young-sang@users.noreply.github.com>
2025-03-07 21:18:42 +0900

    1

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2");
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 
-// dotenv.config();
+dotenv.config();
 
 const connection = mysql.createConnection({
-  
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // 연결 테스트
